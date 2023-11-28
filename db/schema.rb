320ee488e13ad90fffa0d6147a5db77310394761
marioalija <marioalija25@gmail.com>
2023-11-28 14:02:45 -0600

    saving schema

--- db/schema.rb
+++ db/schema.rb
@@ -14,14 +14,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_184245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -33,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_184245) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
 end
