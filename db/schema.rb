b5c8e85ea0a49676fd60dbe8f7a052a3b68f1eaa
marioalija <marioalija25@gmail.com>
2023-11-28 13:58:47 -0600

    made small difference changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.0].define(version: 2023_11_28_184044) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  t.string "title"
-  t.datetime "created_at", null: false
-  t.datetime "updated_at", null: false
-
 ActiveRecord::Schema[7.0].define(version: 2023_11_28_184245) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
