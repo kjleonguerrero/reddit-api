88d4c50b9b9c974d5b49efadf2d23b84f292ee43
bshin27 <125755440+bshin27@users.noreply.github.com>
2023-11-28 13:38:10 -0600

    merged

--- db/schema.rb
+++ db/schema.rb
@@ -15,14 +15,13 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_28_184245) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "title"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
-
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
